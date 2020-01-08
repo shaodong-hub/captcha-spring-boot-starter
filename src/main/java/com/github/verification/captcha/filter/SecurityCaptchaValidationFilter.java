43f397c51f3b74e23cdba2ae09525637c8eb0144
shishaodong <shishaodong@cmhi.chinamobile.com>
2020-01-08 17:40:53 +0800

    新增 验证失败&成功事件

--- src/main/java/com/github/verification/captcha/filter/SecurityCaptchaValidationFilter.java
+++ src/main/java/com/github/verification/captcha/filter/SecurityCaptchaValidationFilter.java
@@ -13,7 +13,6 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.filter.OncePerRequestFilter;
 
